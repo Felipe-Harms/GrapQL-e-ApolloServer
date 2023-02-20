502e49a2d6a1cabe92d6eaac7d893cbd4723b022
Felipe-harms` <Felipe.freitas28@aluno.unip.br`>
2023-02-20 12:06:14 -0300

    run dev funcionando

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import { hello } from './module.js';
 console.log(hello());
+
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+    }
+  `,
+});
+
+server.listen(4000).then(({ url }) => {
+  console.log(`Server Listening  on url ${url}`);
+});
